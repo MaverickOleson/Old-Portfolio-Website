a985f3a00fe33629286b1d8e07e754d1740ca20a
Eric Oleson <moleso587@west-mec.org>
2021-03-24 15:38:52 -0700

    created stylized hear, a navbar filler, and added an image, with a lot of jquery used for resizing each element to fit the viewport

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-
+$(function() {
+    $('header h1').css({'margin-top': $('header h1').height() / 8, 'margin-bottom': $('header h1').height() / 8});
+    $('header nav').css('width', $('header').height() / 5 * 4);
+    $('header nav').css('height', $('header nav').width());
+    $('header nav').css('margin-top', (($('header').outerHeight()) - $('header nav').height()) / 2);
+    $('header nav').css('margin-left', $('header nav').css('margin-top'));
+    $('main').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
+});
+$(window).resize(function() {
+    $('header h1').css({'margin-top': $('header h1').height() / 8, 'margin-bottom': $('header h1').height() / 8});
+    $('header nav').css('width', $('header').height() / 5 * 4);
+    $('header nav').css('height', $('header nav').width());
+    $('header nav').css('margin-top', (($('header').outerHeight()) - $('header nav').height()) / 2);
+    $('header nav').css('margin-left', $('header nav').css('margin-top'));
+    $('main').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
+})
 /*
 Function to animate/reposition elements from navigation to body:
 
