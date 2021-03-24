6897ec71927b7594f9bcfedf45c8769f696c602a
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-03-23 18:19:50 -0700

    Set up html, css, and js files and experimented with nav functionality (difficult)

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+/*
+Function to animate/reposition elements from navigation to body:
+
+$(function(){
+    $('#p1').css('position', 'absolute');
+    $('#p1').animate({top: $('#p-place').offset().top - $('#p-place').height()});
+    //Need to replace elements? Probably not, but if: remove and append
+})*/
