5d8ece5f61e8a2b85a0095585b6d9acd110af215
Eric Oleson <moleso587@west-mec.org>
2021-03-25 15:37:38 -0700

    added text and image and redesigned layout, both using css and jquery

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 $(function() {
     $('header h1').css({'margin-top': $('header h1').height() / 8, 'margin-bottom': $('header h1').height() / 8});
-    $('header nav').css('width', $('header').height() / 5 * 4);
-    $('header nav').css('height', $('header nav').width());
-    $('header nav').css('margin-top', (($('header').outerHeight()) - $('header nav').height()) / 2);
+    $('header nav *').css({'border-right-width': $('header').height() / 4.5 * 2,'border-left-width': $('header').height() / 4.5 * 2});
+    $('header nav').css('margin-top', ($('header').outerHeight() - Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0])) / 2);
     $('header nav').css('margin-left', $('header nav').css('margin-top'));
-    $('main').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
+    $('nav .panel1').css('border-bottom', '' + Math.sqrt(Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2) * 4 - Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2)) + 'px solid salmon');
+    $('nav .panel1 .frontPanel *').css({'left': $('nav').width() / 2 - $('nav .panel1 .frontPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0]) / 2});
+    $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
 });
 $(window).resize(function() {
     $('header h1').css({'margin-top': $('header h1').height() / 8, 'margin-bottom': $('header h1').height() / 8});
-    $('header nav').css('width', $('header').height() / 5 * 4);
-    $('header nav').css('height', $('header nav').width());
-    $('header nav').css('margin-top', (($('header').outerHeight()) - $('header nav').height()) / 2);
+    $('header nav *').css({'border-right-width': $('header').height() / 4.5 * 2,'border-left-width': $('header').height() / 4.5 * 2});
+    $('header nav').css('margin-top', ($('header').outerHeight() - Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0])) / 2);
     $('header nav').css('margin-left', $('header nav').css('margin-top'));
-    $('main').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
+    $('nav .panel1').css('border-bottom', '' + Math.sqrt(Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2) * 4 - Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2)) + 'px solid salmon');
+    $('nav .panel1 .frontPanel *').css({'left': $('nav').width() / 2 - $('nav .panel1 .frontPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0]) / 2});
+    $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
 })
 
 /*
