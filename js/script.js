554741b056980076672b7470b2d8240d8b63f65f
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-04-01 18:21:06 -0700

    restyled navbar (changed nesting), and fixed border-bottom for the nav children

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(function() {
     $('header h1').css({'margin-top': $('header h1').height() / 8, 'margin-bottom': $('header h1').height() / 8});
     $('header nav * *').css({'border-right-width': $('header').height() / 4.5 * 2,'border-left-width': $('header').height() / 4.5 * 2});
-    $('header nav').css('margin-top', ($('header').outerHeight() - Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0])) / 2);
+    $('header nav * *').css('border-bottom-width', Math.sqrt(Math.pow(Number($('header nav * *').css('border-right-width').split('px')[0]), 2) * 4 - Math.pow(Number($('header nav * *').css('border-right-width').split('px')[0]), 2)));
+    $('header nav').css('margin-top', ($('header').outerHeight() - Math.tan(60 * Math.PI / 180) * Number($('header nav * *').css('border-right-width').split('px')[0])) / 2);
     $('header nav').css('margin-left', $('header nav').css('margin-top'));
-    $('header nav * *').css('border-bottom', Math.sqrt(Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2) * 4 - Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2)) + ' solid blue');
-    $('header nav * * *').css({'left': $('nav').width() / 2 - $('nav .panel1 .frontPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0]) / 2});
+    //$('header nav * * *').css({'left': $('header nav').width() / 2 - $('header nav * * *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0]) / 2});
     $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
 });
 $(window).resize(function() {
