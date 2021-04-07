7a91a4d0c0b5765ea12af4b2d2e1217dfaed1ff1
Eric Oleson <moleso587@west-mec.org>
2021-04-07 15:44:35 -0700

    Added extra divs for the nav menu, and added jquery to adjust css transform of those divs

--- js/script.js
+++ js/script.js
@@ -4,9 +4,18 @@ $(function() {
     $('header nav * div').css('border-bottom-width', Math.sqrt(Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]), 2) * 4 - Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]), 2)));
     $('header nav').css('margin-top', ($('header').outerHeight() - Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0])) / 2);
     $('header nav').css('margin-left', $('header nav').css('margin-top'));
-    $('header nav * div *').css({'left': Number($('header nav * div').css('border-right-width').split('px')[0]), 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0]) / 2});
-    $('header nav * .frontPanel').animate({transform: 'rotateY(90deg)'}, 3000);
-    $('header nav * .backPanel').css('animation', 'spin2 7.5s infinite ease-in-out');
+    if((90 - Math.asin(Math.sqrt(Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) * 2, 2) - Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) / Math.cos(1 / 6 * Math.PI), 2)) / Number($('header nav * div').css('border-bottom-width').split('px')[0])) * 180 / Math.PI) === '' + (90 - Math.asin(Math.sqrt(Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) * 2, 2) - Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) / Math.cos(1 / 6 * Math.PI), 2)) / Number($('header nav * div').css('border-bottom-width').split('px')[0])) * 180 / Math.PI)) alert('yes')
+    $('header nav .panel1').css('transform', 'rotateX(' + (90 - Math.asin(Math.sqrt(Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) * 2, 2) - Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) / Math.cos(1 / 6 * Math.PI), 2)) / Number($('header nav * div').css('border-bottom-width').split('px')[0])) * 180 / Math.PI) + 'deg)');
+    $('header nav .panel2').css('transform', 'rotateY(-300deg) rotateX(' + (90 - Math.asin(Math.sqrt(Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) * 2, 2) - Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]) / Math.cos(1 / 6 * Math.PI), 2)) / Number($('header nav * div').css('border-bottom-width').split('px')[0])) * 180 / Math.PI) + 'deg)');
+    $('header nav .panel1 .backPanel *').css({'left': -$('header nav .panel1 .backPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0]) / 2});
+    $('header nav .panel1 .frontPanel *').css({'left': -$('header nav .panel1 .frontPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0]) / 2});
+    $('header nav .panel2 .backPanel *').css({'left': -$('header nav .panel2 .backPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0]) / 2});
+    $('header nav .panel2 .frontPanel *').css({'left': -$('header nav .panel2 .frontPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0]) / 2});
+    $('header nav .panel3 .backPanel *').css({'left': -$('header nav .panel3 .backPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0]) / 2});
+    $('header nav .panel3 .frontPanel *').css({'left': -$('header nav .panel3 .frontPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav * div').css('border-right-width').split('px')[0]) / 2});
+    //$('header nav * .frontPanel').animate({transform: 'rotateY(90deg)'}, 3000);
+    //$('header nav * .backPanel').css('animation', 'spin2 7.5s infinite ease-in-out');
+    //$('header nav * div *').css('transform', 'translateX(' + Number($('header nav * div').css('border-right-width').split('px')[0]) - $('header nav * div *').width() / 2 + ')')
     $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
 });
 /*$(window).resize(function() {
