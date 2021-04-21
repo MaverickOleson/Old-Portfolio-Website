0b42ee9962e72cae6e00c0d5a6e39f7f310a2bd3
Eric Oleson <moleso587@west-mec.org>
2021-04-21 15:40:12 -0700

    Took out some css styling and replaced it with jquery styling (mainly for text). After that, I set up the first 2 sections of the about me page

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ $(function() {
         if(yRotation > 3) yRotation = 1;
         if(dotRotation > 2) dotRotation = 0;
         $('nav .dotDiv .dot').eq(dotRotation).addClass('active');
-        if(yRotation == 1) $('nav .tetrahedron').css('animation', 'spin1 1s linear forwards');
-        if(yRotation == 2) $('nav .tetrahedron').css('animation', 'spin3 1s linear forwards');
-        if(yRotation == 3) $('nav .tetrahedron').css('animation', 'spin5 1s linear forwards');
+        if(yRotation == 1) $('nav .tetrahedron').css('animation', 'spin1 1s ease-in-out forwards');
+        if(yRotation == 2) $('nav .tetrahedron').css('animation', 'spin3 1s ease-in-out forwards');
+        if(yRotation == 3) $('nav .tetrahedron').css('animation', 'spin5 1s ease-in-out forwards');
     }, 5000);
     function yRotateTo(rotateTo){
         if(rotateTo - 1 == yRotation){
@@ -31,7 +31,7 @@ $(function() {
         }
     }
     function rotateY(rotateTo){
-        $('nav .tetrahedron').css('animation', 'spin' + rotateTo + ' 1s linear forwards');
+        $('nav .tetrahedron').css('animation', 'spin' + rotateTo + ' 0.75s ease-in-out forwards');
     }
     $('nav .dotDiv .dot').eq(0).on('click', function(){
         clearInterval(spin);
@@ -51,10 +51,68 @@ $(function() {
         $(this).addClass('active');
         yRotateTo(2);
     });
-    if($(window).width() > $(window).height()) $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
-    else{
-        $('main .row').css('height', 'calc((100vh - ' + $('header').outerHeight() + 'px) * 2)');
+    var fontSize = 0;
+    for(var i = 0; i < $('main .tetrahedron div').length; i++){
+        while($('header .tetrahedron div a h2').eq(i).outerHeight() < $('header .tetrahedron div').eq(i).outerHeight() - $(window).height() / 100 * 5.2){
+            $('header .tetrahedron div a h2').eq(i).css('font-size', fontSize + 'px');
+            fontSize++;
+        }
+        if($('header .title').outerHeight() > $('header').outerHeight() - $(window).height() / 100 * 5.2){
+            fontSize = Number($('header .title').css('font-size').split('px')[0]);
+            while($('header .title').outerHeight() > $('header').outerHeight()  - $(window).height() / 100 * 5.2){
+                $('header .title').css('font-size', fontSize + 'px');
+                fontSize--;
+            }
+        }
     }
+    while($('header .title').outerHeight() < $('header').outerHeight() - $(window).height() / 100 * 5.2){
+        $('header .title').css('font-size', fontSize + 'px');
+        fontSize++;
+    }
+    if($('header .title').outerHeight() > $('header').outerHeight() - $(window).height() / 100 * 5.2){
+        fontSize = Number($('header .title').css('font-size').split('px')[0]);
+        while($('header .title').outerHeight() > $('header').outerHeight()  - $(window).height() / 100 * 5.2){
+            $('header .title').css('font-size', fontSize + 'px');
+            fontSize--;
+        }
+    }
+    fontSize = 0;
+    //$('header .title').css({'padding-right': $('header .title').outerWidth() / 11, 'padding-left': $('header .title').outerWidth() / 11});
+    for(var i = 0; i < $('main .row .textBox').length; i++){
+        while($('main .row .textBox p').eq(i).outerHeight() < $('main .row .textBox').eq(i).height()){
+            $('main .row .textBox p').eq(i).css('font-size', fontSize + 'px');
+            fontSize++;
+        }
+        if($('main .row .textBox p').eq(i).outerHeight() > $('main .row .textBox').eq(i).height()){
+            while($('main .row .textBox p').eq(i).outerHeight() > $('main .row .textBox').eq(i).height()){
+                $('main .row .textBox p').eq(i).css('font-size', fontSize + 'px');
+                fontSize--;
+            }
+        }
+        fontSize = 0;
+    }
+    while($('footer p').outerHeight() < $('footer').outerHeight() - $(window).height() / 100 * 1.7){
+        $('footer p').css('font-size', fontSize + 'px');
+        fontSize++;
+    }  
+    if($('footer p').outerHeight() > $('footer').outerHeight() - $(window).height() / 100 * 1.7){
+        fontSize = Number($('footer p').css('font-size').split('px')[0]);
+        while($('footer p').outerHeight() > $('footer').outerHeight()  - $(window).height() / 100 * 1.7){
+            $('footer p').css('font-size', fontSize + 'px');
+            fontSize--;
+        }
+    }
+    if($(window).width() > $(window).height()){
+        $('main .row div h2').css('font-size', $(window).height() / 100 * 4.5);
+        $('main .row h1').css('font-size', $(window).height() / 100 * 7.1);
+        $('main .row .mainText p').css('font-size', $(window).height() / 100 * 4);
+    }
+    else{
+        $('main .row div h2').css('font-size', $(window).width() / 100 * 6.5);
+        $('main .row h1').css('font-size', $(window).width() / 100 * 7.1);
+        $('main .row .mainText p').css('font-size', $(window).width() / 100 * 4);
+    }
+    //
     //alert(90 - Math.asin(Math.sqrt(Math.pow($('.panel1 .frontPanel').width(), 2) - Math.pow($('.panel1 .frontPanel').width() / 2 / Math.cos(30), 2)) / Math.sqrt(Math.pow($('.panel1 .frontPanel').width(),2) - Math.pow($('.panel1 .frontPanel').width() / 2,2))));
     //$('header nav * div').css({'border-right-width': $('header').height() / 4.5 * 2,'border-left-width': $('header').height() / 4.5 * 2});
     //$('header nav * div').css('border-bottom-width', Math.sqrt(Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]), 2) * 4 - Math.pow(Number($('header nav * div').css('border-right-width').split('px')[0]), 2)));
@@ -73,19 +131,53 @@ $(function() {
     //$('header nav * .backPanel').css('animation', 'spin2 7.5s infinite ease-in-out');
     //$('header nav * div *').css('transform', 'translateX(' + Number($('header nav * div').css('border-right-width').split('px')[0]) - $('header nav * div *').width() / 2 + ')')
 });
-$(window).resize(function() {
-    if($(window).width() > $(window).height()) $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
+jQuery(window).on( "orientationchange", function() {
+    location.reload()
+});
+/*$(window).resize(function() {
+    if($(window).width() > $(window).height()) {
+        $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
+        //$('nav *').css('width', $('nav *').outerWidth() * 2);
+    }
     else{
         $('main .row').css('height', 'calc((100vh - ' + $('header').outerHeight() + 'px) * 2)');
     }
-    /*$('header h1').css({'margin-top': $('header h1').height() / 8, 'margin-bottom': $('header h1').height() / 8});
+    var fontSize = 0;
+    while($('header .title').outerHeight() < $('header').outerHeight() - $(window).height() / 100 * 5.2){
+        $('header .title').css('font-size', fontSize + 'px');
+        fontSize++;
+    }
+    if($('header .title').outerHeight() > $('header').outerHeight() - $(window).height() / 100 * 5.2){
+        fontSize = Number($('header .title').css('font-size').split('px')[0]);
+        while($('header .title').outerHeight() > $('header').outerHeight()  - $(window).height() / 100 * 5.2){
+            $('header .title').css('font-size', fontSize + 'px');
+            fontSize--;
+        }
+    }
+    fontSize = 0;
+    //$('header .title').css({'padding-right': $('header .title').outerWidth() / 11, 'padding-left': $('header .title').outerWidth() / 11});
+    for(var i = 0; i < 2; i++){
+        while($('main .row .textBox p').eq(i).outerHeight() < $('main .row .textBox').eq(i).height()){
+            $('main .row .textBox p').eq(i).css('font-size', fontSize + 'px');
+            fontSize++;
+        }
+        if($('main .row .textBox p').eq(i).outerHeight() > $('main .row .textBox').eq(i).height()){
+            fontSize = Number($('main .row .textBox p').eq(i).css('font-size').split('px')[0]);
+            while($('main .row .textBox p').eq(i).outerHeight() > $('main .row .textBox').eq(i).height()){
+                $('main .row .textBox p').eq(i).css('font-size', fontSize + 'px');
+                fontSize--;
+            }
+        }
+        fontSize = 0;
+    }
+    $('header h1').css({'margin-top': $('header h1').height() / 8, 'margin-bottom': $('header h1').height() / 8});
     $('header nav *').css({'border-right-width': $('header').height() / 4.5 * 2,'border-left-width': $('header').height() / 4.5 * 2});
     $('header nav').css('margin-top', ($('header').outerHeight() - Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0])) / 2);
     $('header nav').css('margin-left', $('header nav').css('margin-top'));
     $('nav .panel1').css('border-bottom', '' + Math.sqrt(Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2) * 4 - Math.pow(Number($('nav .panel1').css('border-right-width').split('px')[0]), 2)) + 'px solid salmon');
     $('nav .panel1 .frontPanel *').css({'left': $('nav').width() / 2 - $('nav .panel1 .frontPanel *').width() / 2, 'top': Math.tan(60 * Math.PI / 180) * Number($('header nav *').css('border-right-width').split('px')[0]) / 2});
-    $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');*/
-})
+    $('main .row').css('height', 'calc(100vh - ' + $('header').outerHeight() + 'px)');
+})*/
 
 /*
 Function to animate/reposition elements from navigation to body:
